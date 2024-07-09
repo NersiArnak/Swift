d85b31b3d277e56406fe1e64d4bb5005e6f51d8e
nersiarnak <arnonersi777@gmail.com>
2024-07-09 22:22:06 +0300

    done hw1 sk

---
+++ skutorenkoKurs/homeWork/hw1/homeWork1.playground/Contents.swift
@@ -0,0 +1,7 @@
+let firstName = "Имя"
+let lastName = "Фамилия"
+let fatherName = "Отчество"
+let height = "Рост"
+let weight = "Вес"
+let age = "Возраст"
+print("\(firstName) - Арнак\n\(lastName) - Нерсисян" + "\n\(fatherName) - Гарикович \n\(height) - 174см \n\(weight) - 67кг \n\(age) - 20 лет")
