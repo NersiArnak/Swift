86696a3a65054050c1158a988b75ab40afa128eb
nersiarnak <arnonersi777@gmail.com>
2024-11-19 23:47:14 +0300

    done tic tac toe

--- skutorenkoKurs/repetition/subscripts/Subscripts.playground/Contents.swift
+++ skutorenkoKurs/repetition/subscripts/Subscripts.playground/Contents.swift
@@ -1,3 +1,123 @@
+//MARK: home work
+print("№1 game - Chess\n")
+struct GameChess {
+    private func letterToNum(line: Character) -> (value: Int, count: Int) {
+        let alphabet = "abcdefgh"
+        var num = 0
+        var index = 1
+        var count = alphabet.count
+        for i in alphabet {
+            if i == Character(line.lowercased()) {
+                num = index
+            }
+        }
+        return (num, count)
+    }
+    
+    enum Color : String {
+        case white = "White"
+        case black = "Black"
+    }
+    
+    subscript(line: Character, column: Int) -> String? {
+        var color = Color.black
+        let newLine = letterToNum(line: line).value
+        let count = letterToNum(line: line).count
+        if column > count || column <= 0 {
+            return nil
+        }
+        
+        if (newLine + column) % 2 == 0 {
+            color = Color.white
+        }
+        return color.rawValue
+    }
+
+}
+
+var point = GameChess()
+point["A", 2]
+
+print("№2 game - TicTacToe")
+class TicTacToe {
+    var height : Int
+    var width : Int
+    var board : [[Character]]
+    
+    init(height: Int, width: Int) {
+        self.height = height
+        self.width = width
+        self.board = Array(repeating: Array(repeating: "⬜️", count: width), count: height)
+    }
+    
+    func drowPrint(board: inout [[Character]]) {
+        let lastHeight = height - 1
+        let lastWidth = width - 1
+        let side : Character = "⬛️"
+        for i in 0..<board.count {
+            for j in 0..<board[i].count {
+                board[0][j] = side
+                board[i][0] = side
+                board[lastHeight][j] = side
+                board[i][lastWidth] = side
+                print(board[i][j], terminator: " ")
+            }
+            print()
+        }
+    }
+    
+    enum Value : Character {
+        case cross = "❌"
+        case circle = "⭕️"
+    }
+    
+    subscript(x: Int, y: Int, value: Value) -> Character? {
+        let lastHeight = height - 1
+        let lastWidth = width - 1
+        
+        if x < 0 || x > lastHeight || y < 0 || y > lastWidth {
+            print("[\(x),\(y)] - неверное значение")
+            return nil
+        }
+        if board[x][y] != "⬜️" {
+            print("Клетка занята")
+            return nil
+        }
+        else {
+            board[x][y] = value.rawValue
+        }
+        
+        let saveValue = board[x][y]
+        let top = x - 1, twoTop = x - 2
+        let left = y - 1, twoLeft = y - 2
+        let down = x + 1, twoDown = x + 2
+        let right = y + 1, twoRight = y + 2
+        
+        let winner = "Игра окончена, выиграл игрок, который играл за \(saveValue)"
+
+        if (board[top][y] == saveValue && board[down][y] == saveValue) || (board[x][left] == saveValue && board[x][right] == saveValue) {
+            print(winner)
+        }
+        
+        if (board[down][y] == saveValue && board[twoDown][y] == saveValue) || (board[top][y] == saveValue && board[twoTop][y] == saveValue) {
+            print(winner)
+        }
+        
+        
+        if (board[x][right] == saveValue && board[x][twoRight] == saveValue) || (board[x][left] == saveValue && board[x][twoLeft] == saveValue) {
+            print(winner)
+        }
+    
+        return board[x][y]
+    }
+}
+
+let board = TicTacToe(height: 8, width: 8)
+board[2, 1, .cross]
+board[2, 2, .cross]
+board[2, 3, .cross]
+board.drowPrint(board: &board.board)
+
 // MARK: class work
 let array = ["a", "b", "c"]
 array[0]
@@ -80,109 +200,3 @@ struct Field {
 var field = Field()
 field["a", 5]
 
-//MARK: home work
-print("№1 game - chess\n")
-struct GameChess {
-    private func letterToNum(line: Character) -> (value: Int, count: Int) {
-        let alphabet = "abcdefgh"
-        var num = 0
-        var index = 1
-        var count = alphabet.count
-        for i in alphabet {
-            if i == Character(line.lowercased()) {
-                num = index
-            }
-        }
-        return (num, count)
-    }
-    
-    enum Color : String {
-        case white = "White"
-        case black = "Black"
-    }
-    
-    subscript(line: Character, column: Int) -> String? {
-        var color = Color.black
-        let newLine = letterToNum(line: line).value
-        let count = letterToNum(line: line).count
-        if column > count || column <= 0 {
-            return nil
-        }
-        
-        if (newLine + column) % 2 == 0 {
-            color = Color.white
-        }
-        return color.rawValue
-    }
-
-}
-
-var point = GameChess()
-point["A", 2]
-
-print("№2 game - tic tac toe")
-class TicTacToe {
-    var height : Int
-    var width : Int
-    var board : [[Character]]
-    
-    
-    init(height: Int, width: Int) {
-        self.height = height
-        self.width = width
-        self.board = Array(repeating: Array(repeating: "⬜️", count: width), count: height)
-    }
-    
-    func drowPrint(board: inout [[Character]]) {
-        let lastHeight = height - 1
-        let lastWidth = width - 1
-        let side : Character = "⬛️"
-        for i in 0..<board.count {
-            for j in 0..<board[i].count {
-                board[0][j] = side
-                board[i][0] = side
-                board[lastHeight][j] = side
-                board[i][lastWidth] = side
-                print(board[i][j], terminator: " ")
-            }
-            print()
-        }
-    }
-    
-    enum Value : Character {
-        case empty = "⬜️"
-        case cross = "❌"
-        case circle = "⭕️"
-    }
-    
-    subscript(x: Int, y: Int, value: Value) -> Character? {
-        let lastHeight = height - 1
-        let lastWidth = width - 1
-        if x < 0 || x > lastHeight || y < 0 || y > lastWidth {
-            print("[\(x),\(y)] - неверное значение")
-            return nil
-        }
-        if board[x][y] != "⬜️" {
-            print("Клетка занята")
-            return nil
-        }
-        else {
-            switch value {
-                case .empty:
-                board[x][y] = value.rawValue
-            case .cross:
-                board[x][y] = value.rawValue
-            case .circle:
-                board[x][y] = value.rawValue
-            }
-        }
-        return board[x][y]
-    }
-}
-
-let board = TicTacToe(height: 8, width: 8)
-board[10, 2, .cross]
-board[3, 2, .cross]
-board[1, 2, .circle]
-board[1, 2, .circle]
-board.drowPrint(board: &board.board)
